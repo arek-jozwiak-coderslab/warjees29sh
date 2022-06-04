c6bcbd6d8301ad131f3a4ced3c19088c3fc8366e
arek jozwiak <arkadiusz.jozwiak@coderslab.pl>
2022-06-04 09:01:06 +0200

    fix publisher

--- src/main/java/pl/coderslab/book/BookRepository.java
+++ src/main/java/pl/coderslab/book/BookRepository.java
@@ -1,9 +1,11 @@
 package pl.coderslab.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -16,4 +18,19 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     //#3
     List<Book> findByCategoryId(long id);
+
+    @Query("select b from Book b where b.title=?1")
+    List<Book> findByOne(String title);
+
+
+    @Query("select b from Book b where b.category=?1")
+    List<Book> findBySecond(Category category);
+
+    List<Book> findByRating(int rating);
+
+    List<Book> findByAuthorsIn(List<Author> authors);
+
+    List<Book> findByPublish(Publisher publisher);
+
+
 }
--- src/main/java/pl/coderslab/book/Publisher.java
+++ src/main/java/pl/coderslab/book/Publisher.java
@@ -4,14 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Publisher {
-
     @Id
-    @GeneratedValue(strategy =
-            GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min = 5)
     private String name;
 
     public Long getId() {
